62d1aabe435622a497a6802e560798fe913958f0
Wyatt Allan <wyatt.allan@securitymetrics.com>
2021-04-09 15:40:04 -0600

    changes

--- src/schema.js
+++ src/schema.js
@@ -53,63 +53,6 @@ const Query = objectType({
         })
       },
     })
-
-    // t.nonNull.list.nonNull.field('feed', {
-    //   type: 'Post',
-    //   args: {
-    //     searchString: stringArg(),
-    //     skip: intArg(),
-    //     take: intArg(),
-    //     orderBy: arg({
-    //       type: 'PostOrderByUpdatedAtInput',
-    //     }),
-    //   },
-    //   resolve: (_parent, args, context) => {
-    //     const or = args.searchString
-    //       ? {
-    //           OR: [
-    //             { title: { contains: args.searchString } },
-    //             { content: { contains: args.searchString } },
-    //           ],
-    //         }
-    //       : {}
-
-    //     return context.prisma.post.findMany({
-    //       where: {
-    //         published: true,
-    //         ...or,
-    //       },
-    //       take: args.take || undefined,
-    //       skip: args.skip || undefined,
-    //       orderBy: args.orderBy || undefined,
-    //     })
-    //   },
-    // })
-
-    // t.list.field('draftsByUser', {
-    //   type: 'Post',
-    //   args: {
-    //     userUniqueInput: nonNull(
-    //       arg({
-    //         type: 'UserUniqueInput',
-    //       }),
-    //     ),
-    //   },
-    //   resolve: (_parent, args, context) => {
-    //     return context.prisma.user
-    //       .findUnique({
-    //         where: {
-    //           id: args.userUniqueInput.id || undefined,
-    //           email: args.userUniqueInput.email || undefined,
-    //         },
-    //       })
-    //       .posts({
-    //         where: {
-    //           published: false,
-    //         },
-    //       })
-    //   },
-    // })
   },
 })
 
